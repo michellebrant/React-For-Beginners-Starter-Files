47c7599c83e41cdfd46bdb518c16d23fcd5a90d6
Michelle Brant <michellebrant>
2016-12-26 11:48:45 -0500

    few extra for video 7

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -4,8 +4,13 @@ class Header extends React.Component {
   render(){
     return(
    <header className="top">
-   <h1>Catch of the day</h1>
-   <h5 className="tagline">{this.props.tagline}</h5>
+   <h1>Catch
+   <span className="ofThe">
+   <span className="of">of</span>
+   <span className="the">the</span>
+   </span>
+   day</h1>
+   <h5 className="tagline"><span>{this.props.tagline}</span></h5>
 
 
    </header>
