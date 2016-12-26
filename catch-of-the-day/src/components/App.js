6e9f7d81fc538d34d3dbd4cfdcf2cfcf1c1020de
Michelle Brant <michellebrant>
2016-12-26 11:46:30 -0500

    work from video 7

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     return(
       <div className='catch-of-the-day'>
       <div className="menu">
-      <Header />
+      <Header age="5000" tagline="Fresh Seafood Market"/>
 
       </div>
       <Order />
--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+//this.props.tagline is how we insert variables in jsx
 class Header extends React.Component {
   render(){
     return(
-    <p>Header</p>
+   <header className="top">
+   <h1>Catch of the day</h1>
+   <h5 className="tagline">{this.props.tagline}</h5>
+
+
+   </header>
       )
   }
 
