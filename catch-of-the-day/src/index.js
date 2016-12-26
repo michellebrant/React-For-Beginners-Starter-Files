49226dfd3bb0c059bf598674b901f73633224421
Michelle Brant <michellebrant>
2016-12-26 11:11:40 -0500

    video 3 work

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,8 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom';
+//importing the render method from react dom library^
+
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main'))
