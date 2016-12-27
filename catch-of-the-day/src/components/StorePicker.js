6aa42219495315d52f26a3590435b8836d96c1e8
Michelle Brant <michellebrant>
2016-12-27 18:16:48 -0500

    video 11

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,6 +2,18 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore(event) {
+    //constructor of the component to get this work!
+    // constructor() {
+    //   super();
+    //   this.goToStore = this.goToStore.bind(this);
+    // }
+      event.preventDefault();
+      console.log(this.storeInput.value)
+    //first grab the text from the box
+
+    //second transition from / to /store/:storID
+  }
   render() {
     // clunky way to do this
     // return React.createElement('p', {clsasName:'testing'}, 'i love you')
@@ -14,10 +26,10 @@ class StorePicker extends React.Component {
 
 
     return (
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
       {/* THIS IS THE ONLY WAY TO COMMENT IN JSX ITS ANNOYING also CANNOT be on top - can't be the first thing in the jsx*/}
         <h2>Please enter a store</h2>
-        <input type='text' required placeholder='Store Name' defaultValue={getFunName()}/>
+        <input type='text' required placeholder='Store Name' defaultValue={getFunName()} ref={(input => {this.storeInput = input})}/>
         <button type="submit">Visit store</button>
 
         </form>
