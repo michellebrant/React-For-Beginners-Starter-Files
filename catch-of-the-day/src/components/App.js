32761b246a2abc01179deeedb58ec7f1e2434407
Michelle Brant <michellebrant>
2016-12-28 10:07:15 -0500

    add video 17

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -71,7 +71,7 @@ addToOrder(key){
       }
       </ul>
       </div>
-      <Order />
+      <Order fishes={this.state.fishes} order={this.state.order} />
       <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
 
